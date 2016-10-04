e6e99ba2cc16ba6db52bce55188a20ad1e2fdc0e
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-10-04 15:32:08 +0200

    Trim down OB

--- pyinstaller_linux.sh
+++ pyinstaller_linux.sh
@@ -9,7 +9,11 @@ pyinstaller \
     --exclude-module Tkinter \
     --exclude-module rdkit \
     --add-data "${CONDA_ROOT}/share/openbabel/2.4.0/*:data/" \
-    --add-binary "${CONDA_ROOT}/lib/openbabel/2.4.0/*.so:." \
+    --add-binary "${CONDA_ROOT}/lib/openbabel/2.4.0/mdlformat.so:." \
+    --add-binary "${CONDA_ROOT}/lib/openbabel/2.4.0/mol2format.so:." \
+    --add-binary "${CONDA_ROOT}/lib/openbabel/2.4.0/pdbformat.so:." \
+    --add-binary "${CONDA_ROOT}/lib/openbabel/2.4.0/pdbqtformat.so:." \
+    --add-binary "${CONDA_ROOT}/lib/openbabel/2.4.0/smilesformat.so:." \
     --add-binary "${CONDA_ROOT}/lib/libinchi.so.0:." \
     --runtime-hook pyi_rth_obdata.py \
     --add-data=${CONDA_ROOT}/lib/python2.7/site-packages/oddt/scoring/functions/RFScore/*.csv:oddt/scoring/functions/RFScore/ \
