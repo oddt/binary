17dce89a1b5b880166ec3350d61172cb7d2b0a8d
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-10-06 15:14:34 +0200

    MinGW32

--- hook-openbabel.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import get_gi_typelibs, is_darwin
-
-binaries, datas, hiddenimports = [], [], []
-if is_darwin:
-    binaries, datas, hiddenimports = get_gi_typelibs('cairo', '2')
