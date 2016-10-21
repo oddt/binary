60c23fd4f4194449317a9dc320a0e9df6b69e5f7
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-10-21 11:00:56 +0200

    Fix Mac

--- pyinstaller_macos.sh
+++ pyinstaller_macos.sh
@@ -24,7 +24,7 @@ pyinstaller \
     --nowindow \
     --strip \
     --add-binary "${CONDA_ROOT}/lib/libinchi.0.dylib:." \
-    --add-binary "${CONDA_ROOT}/lib/libmkl_avx.dylib:." \
+    --add-binary "${CONDA_ROOT}/lib/libmkl_mc.dylib:." \
     --add-binary "/opt/X11/lib/libcairo.2.dylib:." \
     --add-binary "/opt/X11/lib/libxcb-render.0.dylib:." \
     --add-binary "/opt/X11/lib/libxcb-shm.0.dylib:." \
